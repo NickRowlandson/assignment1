543f644fec08d518a3903e87e6fc6d361f9e7e42
[Nick] <[nicholasrowlandson@gmail.com]>
2015-10-05 22:46:36 -0400

    express generated and angular install

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'First Express Generated Site', 
+                       message: 'the first page'});
 });
 
 module.exports = router;
--- app/routes/users.js
+++ app/routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+    
+    //load a user view instead
+    res.render('users');
 });
 
 module.exports = router;
